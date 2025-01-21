690748ed663097bdda7dd8fd635bff0d1ea9780d
mapxn <16299874+mapxn@users.noreply.github.com>
2025-01-21 14:51:57 +0800

    init version

---
+++ README.md
@@ -0,0 +1,6 @@
+# waline-server-template 
+
+Waline 的服务端部署代码，使用Vercel部署。
+
+可以根据需要加载Waline插件，插件安装方式：修改 `index.ejs` 和 `packages.json` 文件。
+
---
+++ index.cjs
@@ -0,0 +1,9 @@
+const Application = require('@waline/vercel');
+
+
+module.exports = Application({
+  plugins: [],
+  async postSave(comment) {
+    // do what ever you want after comment saved
+  },
+});
